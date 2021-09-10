1710122752653a10afd56002949ef5aeafc12885
yousraRB <yousrarebai886@gmail.com>
2021-09-10 22:59:01 +0200

    maximum minumn f les piles

--- les piles/maximum.c
+++ les piles/maximum.c
@@ -2,87 +2,109 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include "piles.h"
-int compteur (PILE *tete);
+#include<time.h>
+void affichage (PILE *tete);
+void supprime (PILE **tete,int val);
 int max (PILE *tete);
-PILE* supprime(PILE *tete,int val);
+int min (PILE *tete);
 int main (){
-	
+	int i,nbr;
 	PILE *tete;
-	int i=0,val,choix=1,nbr;
 	
+	srand(time(NULL));
+	init(&tete);
 	
-	init (&tete);
-	while (choix==1){
-		printf (" dekhel la valeur %d \n",i);
-		scanf ("%d",&val);
-		enpiler(&tete,val);
-		do{
-			printf (" habit tzid valeur aktb 1 sinon kteb 0 \n");
-			scanf("%d",&choix);
-		}while (choix!=0 &&choix!=1);
-		i++;
+	for(i=0;i<5;i++){
+		nbr=rand()%30;
+		enpiler(&tete,nbr);
 	}
-	nbr=compteur(tete);
-	printf ("lpile fiha %d valeur \n",nbr);
-	
+	affichage (tete);
+	printf("la valeur lihab tsuprimiha : \n");
+	scanf("%d",&nbr);
+	supprime(&tete,nbr);
+	printf ("hawlik suprisentk \n");
+	affichage (tete);
 	nbr=max(tete);
-
-	printf("lmaximum t3 lpile is %d \n",nbr);
-	printf (" give me number lihabit tsuprimih \n");
-	scanf ("%d",&nbr);
-	tete=supprime (tete,nbr);
-	printf (" kisuprimito : \n");
-	while (!vide(tete)){
-		depiler(&tete,&nbr);
-		printf ("%d -|",nbr);
-	}
+	printf (" lmaximum f hed la pile is %d \n",nbr);
+	printf("*lisatatk* \n");
+	affichage (tete);
+	nbr=min(tete);
+	printf("hwlik sghayarr fihom : %d \n",nbr);
+	printf("*lisatatk* \n");
+	affichage (tete);
+	
 }
-int compteur (PILE *tete){
-	int i=0;
-	PILE *p;
+void affichage (PILE *tete){
+	PILE *r;
 	int val;
-	p=tete;
-	init (&tete);
-	while (!vide(p)){
-		i++;
-		depiler(&p,&val);
-		enpiler(&tete,val);
-		
-		
+	
+	init(&r);
+	while (!vide(tete)){
+		depiler(&tete,&val);
+		printf("%d \n",val);
+		enpiler (&r,val);
 	}
-	return i;
+	init(&tete);
+
+	while (!vide(r)){
+		depiler(&r,&val);
+		enpiler (&tete,val);
+	}
+	
+}
+void supprime (PILE **tete,int val){
+	PILE *r;
+	int sup;
+	
+	init (&r);
+	while (!vide(*tete)){
+		depiler(&(*tete),&sup);
+		if (sup!=val){
+			enpiler(&r,sup);
+		
+		}
+	}
+	init (&(*tete));
+	while(!vide(r)){
+		depiler(&r,&sup);
+		enpiler(&(*tete),sup);
+	}
 }
 int max (PILE *tete){
-	int max,val;
-	PILE *p;
-	p=tete;
-	init(&tete);
-	depiler(&p,&max);
-	enpiler(&tete,max);
-	
-	while(!vide(p)){
-		depiler(&p,&val);
-		enpiler(&tete,val);
-		if(val>max)
+	PILE *r;
+	int val , max;
+	init (&r);
+	depiler(&(tete),&max);
+	enpiler(&r,max);
+	while (!vide(tete)){
+		depiler(&(tete),&val);
+		enpiler (&r,val);
+		if (val >max)
 			max=val;
-		printf("%d |%d \n",max,val);
+	}
+	init (&tete);
+	while (!vide(r)){
+		depiler(&r,&val);
+		enpiler(&tete,val);
 	}
 	return max;
 }
-PILE * supprime(PILE *tete,int val){
-	int x;
+int min (PILE *tete){
 	PILE *r;
-	init (&r);
-	while (!vide(tete)){
-		depiler(&tete,&x);
-		if(x!=val){
-			enpiler(&r,x);
-		}
+	int val,min;
+	init(&r);
+	depiler(&tete,&min);
+	enpiler(&r,min);
+	while(!vide(tete)){
+		depiler(&tete,&val);
+		enpiler(&r,val);
+		if(val<min)
+			min=val;
 	}
-	init(&tete);
-	while (!vide(r)){
-		depiler(&r,&x);
-		enpiler(&tete,x);
+	init (&tete);
+	while(!vide(r)){
+		depiler(&r,&val);
+		enpiler(&tete,val);
 	}
-	return tete;
+	return min;
 }
